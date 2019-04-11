d0d2fa77cd20cd15103f1e7a501ccd28d02d99ee
qmqd1225 <qmqd1225@gmail.com>
2019-04-11 18:00:19 +1200

    update

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {UserDetailService} from '../../user-detail.service';
+import {UserDetail} from '../../models/UserDetail';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  detail: any;
+  // tslint:disable-next-line:variable-name
+  constructor(protected _service: UserDetailService) { }
 
   ngOnInit() {
+    const a = new UserDetail();
+    a.password = 'qwe';
+    a.username = 'qwe';
+    this._service.getUserDetail(a).subscribe(data => {
+      // setting;
+    });
   }
 
 }
---
+++ src/app/models/UserDetail.ts
@@ -0,0 +1,4 @@
+export class UserDetail {
+    username: string;
+    password: string;
+}
