2528882f694d93044aecf5ebe4f17b29564ce295
qmqd1225 <qmqd1225@gmail.com>
2019-04-18 11:00:24 +1200

    Chris environment

--- src/app/services/user-detail.service.ts
+++ src/app/services/user-detail.service.ts
@@ -2,38 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserDetail } from '../models/UserDetail';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserDetailService {
-  // tslint:disable-next-line:variable-name
-  private _url = 'http://192.168.178.76:5000/api/getrole';
+  private urlLocal: any = environment._url;
   // tslint:disable-next-line:variable-name
   constructor(private _http: HttpClient) { }
 
   getUserDetail(usrAndPass): Observable<any> {
-    return this._http.post<UserDetail>(this._url, usrAndPass);
+    return this._http.post<UserDetail>(this.urlLocal, usrAndPass);
   }
 }
-<<<<<<< HEAD:src/app/user-detail.service.ts
-=======
-
-export class LearnersListService {
-  // tslint:disable-next-line:variable-name
-  private _url = 'http://192.168.178.76:5000/api/';
-
-  constructor(private http: HttpClient) {}
-
-  getLearners(name): Observable<any[] > {
-    return this.http.get<any[]>(this._url + 'learner/' + name);
-  }
-  getInvoice(id): Observable<any[] > {
-    return this.http.get<any[]>(this._url + 'payment/invoice/' + id);
-  }
-  addFund(fund) {
-    return this.http.post(this._url + 'payment/payInvoice', fund, {responseType: 'text'});
-  }
-
-}
->>>>>>> 3117cf1aa4f23871a5e1bb30decedd3e2a15e023:src/app/services/user-detail.service.ts
