d2381733495ff0898d3556e8faaaf58d411c16d6
Mike <info@learnspace.co.nz>
2019-05-01 10:17:14 +1200

    update

--- src/app/components/basic/login/login.component.ts
+++ src/app/components/basic/login/login.component.ts
@@ -26,7 +26,8 @@ export class LoginComponent implements OnInit {
     private authenticationService: AuthenticationService,
     private alertService: AlertService
   ) {
-    // redirect to home if already logged in
+    // redirect to home if already logged in 
+    // Delete this part 
     if (this.authenticationService.currentUserValue) {
       this.router.navigate(['/']);
     }
