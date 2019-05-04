f25eeba7b9b09f303c2ac59a6e891fab5f1c410b
StellaRu <stella_ru@outlook.com>
2019-05-04 23:29:36 +1200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
       ]},
       { path: 'payment/other', component: AdminLearnerPaymentOtherComponent },      
       { path: 'teacher', component: BodyComponent},
-      { path: 'learner/registration', component: LearnerRegistrationFormComponent },
+      { path: 'learner', component: LearnerRegistrationFormComponent },
       { path: 'timePicker', component: TimePickerComponent }
     ]
   },
