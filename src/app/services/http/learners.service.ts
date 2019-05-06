02bd68d997c6a050cbc340e166b16d9df2887ed5
Mike <info@learnspace.co.nz>
2019-05-06 16:01:11 +1200

    Update Mike

--- src/app/services/http/learners.service.ts
+++ src/app/services/http/learners.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { catchError } from 'rxjs/operators';
@@ -10,10 +10,18 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class LearnersService {
-
+  httpHeaders: HttpHeaders;
+  token:string
+  
   private baseUrl: any = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
+
+  // API Request headers
+  prepareHeaders(){
+    this.token = localStorage.getItem('Token')
+    this.httpHeaders = new HttpHeaders({'Authorization': ""+ localStorage.getItem('Token')})
+  }
 
   getLearners(name) {
   return this.http.get<any[]>(this.baseUrl + 'learner/' + name)
