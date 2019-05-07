81cb0028765a0df2f8c71fa21470fe45637a9110
Mike <info@learnspace.co.nz>
2019-05-08 11:12:54 +1200

    update

--- src/app/services/http/learners.service.ts
+++ src/app/services/http/learners.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 import { catchError } from 'rxjs/operators';
 // import 'rxjs/add/operator/catch';
 // import 'rxjs/add/observable/throw';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://45.76.123.59:5000/api',
+  // baseUrl: 'http://45.76.123.59:5000/api/',
+  baseUrl: 'http://192.168.178.76:5000/api/',
+  companyName: 'Able Music Studio',
+  siteName: 'Project Pegasus'
 };
