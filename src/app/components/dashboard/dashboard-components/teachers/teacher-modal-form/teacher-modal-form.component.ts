7fbf2358e6b34ae25e314d3e71abf63103916248
GIJoeRambo <richardnzworking@gmail.com>
2019-05-14 00:32:47 +1200

    V5 update

--- src/app/components/dashboard/dashboard-components/teachers/teacher-modal-form/teacher-modal-form.component.ts
+++ src/app/components/dashboard/dashboard-components/teachers/teacher-modal-form/teacher-modal-form.component.ts
@@ -22,10 +22,10 @@ export class TeacherModalFormComponent implements OnInit {
   public showRightImgFlag: boolean = false;
   public photoToSubmit: any;
   public idPhotoToSubmit: any;
-  public week: Array<string> = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satday', 'Sunday'];
+  public week: Array<string> = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   public idTypeList = [{ 'idTypeId': 1, 'idTypeName': 'Driver Lisence' },
-                       { 'idTypeId': 2, 'idTypeName': '18+' },
-                       { 'idTypeId': 3, 'idTypeName': 'Passport' }];
+  { 'idTypeId': 2, 'idTypeName': '18+' },
+  { 'idTypeId': 3, 'idTypeName': 'Passport' }];
 
   @Input() command;
   @Input() whichTeacher;
@@ -41,116 +41,6 @@ export class TeacherModalFormComponent implements OnInit {
     this.getDropdownOptions();
   }
 
-  /////////////////////////////////////////////methods call by HTML Element or Event/////////////////////////////////////////////////
-
-  /*
-    display default language selection
-  */
-  isLanguageCheck(LanguageId) {
-    if (this.command !== 0) {
-      for (let i of this.whichTeacher.TeacherLanguage) {
-        if (LanguageId == i.LangId) {
-          return true;
-        }
-      }
-      return false;
-    }
-    else {
-      return false;
-    }
-  }
-
-  /*
-    switch photo and IdPhoto
-  */
-  showPhotos(position) {
-    let leftImgObj = document.getElementById('img_left');
-    let rightImgObj = document.getElementById('img_right');
-
-    if (position == 0) {
-      this.showLeftImgFlag = true;
-      this.showRightImgFlag = false;
-
-      rightImgObj.style.display = 'none';
-      leftImgObj.style.display = 'block';
-    }
-    else {
-      this.showRightImgFlag = true;
-      this.showLeftImgFlag = false;
-
-      leftImgObj.style.display = 'none';
-      rightImgObj.style.display = 'block';
-    }
-  }
-
-  /*
-    get days that teacher available
-  */
-  getAvailableDays() {
-    let availableDays: Array<number> = []
-    if (this.command == 1 && this.whichTeacher.AvailableDays.length !== 0) {
-      for (let i of this.whichTeacher.AvailableDays) {
-        if (availableDays.indexOf(i.DayOfWeek) == -1) {
-          availableDays.push(i.DayOfWeek)
-        }
-      }
-    }
-    return availableDays;
-  }
-
-  /*
-    get branches that teacher available in specfy date
-  */
-  getAvailableOrgs(whichDay) {
-    if (this.command == 1 && this.whichTeacher.AvailableDays.length !== 0) {
-      let temOrgs = [];
-      for (let i of this.whichTeacher.AvailableDays) {
-        if (i.DayOfWeek == whichDay) {
-          if (temOrgs.indexOf(i.OrgId) == -1) {
-            temOrgs.push(i.OrgId);
-          }
-        }
-      }
-      return temOrgs;
-    }
-  }
-
-  /*
-    pre view the img that user upload
-  */
-  preViewImg(event, whichPhoto) {
-    let photoObj;
-    let photoRender;
-    if (whichPhoto == 0) {
-      photoObj = document.getElementById('userPhoto');
-      this.photoToSubmit = <File>event.target.files[0];
-      photoRender = this.photoToSubmit;
-    }
-    else {
-      photoObj = document.getElementById('userIdPhoto');
-      this.idPhotoToSubmit = <File>event.target.files[0];
-      photoRender = this.idPhotoToSubmit;
-    }
-
-    let reader = new FileReader();
-    reader.onloadend = function () {
-      photoObj.setAttribute("src", this.result.toString());
-    }
-    reader.readAsDataURL(photoRender);
-  }
-
-  
-  showBranchOptions(event, i) {
-    let dropDownObj = document.getElementById(i);
-    event.target.attributes.flag = !event.target.attributes.flag;
-
-    if (event.target.attributes.flag == true) {
-      dropDownObj.style.display = 'block';
-    }
-    else {
-      dropDownObj.style.display = 'none';
-    }
-  }
   /////////////////////////////////////////////methods call by other methods/////////////////////////////////////////////////
 
   /*
@@ -205,7 +95,129 @@ export class TeacherModalFormComponent implements OnInit {
     }
   }
 
- /////////////////////////////////////////////form group/////////////////////////////////////////////////
+  /////////////////////////////////////////////methods call by HTML Element or Event/////////////////////////////////////////////////
+
+  /*
+    display default language selection
+  */
+  setDefaultLanguageSelection(LanguageId) {
+    if (this.command !== 0) {
+      for (let i of this.whichTeacher.TeacherLanguage) {
+        if (LanguageId == i.LangId) {
+          return true;
+        }
+      }
+      return false;
+    }
+    else {
+      return false;
+    }
+  }
+
+  /*
+    switch photo and IdPhoto
+    and change them styles
+  */
+  showPhotos(position) {
+    let leftImgObj = document.getElementById('img_left');
+    let rightImgObj = document.getElementById('img_right');
+
+    if (position == 0) {
+      this.showLeftImgFlag = true;
+      this.showRightImgFlag = false;
+
+      rightImgObj.style.display = 'none';
+      leftImgObj.style.display = 'block';
+    }
+    else {
+      this.showRightImgFlag = true;
+      this.showLeftImgFlag = false;
+
+      leftImgObj.style.display = 'none';
+      rightImgObj.style.display = 'block';
+    }
+  }
+
+  /*
+    in update and add mode
+    get days that teacher available of one week
+  */
+  getAvailableDays() {
+    let availableDays: Array<number> = []
+    if (this.command == 1 && this.whichTeacher.AvailableDays.length !== 0) {
+      for (let i of this.whichTeacher.AvailableDays) {
+        if (availableDays.indexOf(i.DayOfWeek) == -1) {
+          availableDays.push(i.DayOfWeek)
+        }
+      }
+    }
+    return availableDays;
+  }
+
+  /*
+    get branches that teacher available in specfy date
+  */
+  getAvailableOrgs(whichDay) {
+    if (this.command == 1 && this.whichTeacher.AvailableDays.length !== 0) {
+      let temOrgs = [];
+      for (let i of this.whichTeacher.AvailableDays) {
+        if (i.DayOfWeek == whichDay && temOrgs.indexOf(i.OrgId) == -1) {
+          temOrgs.push(i.OrgId);
+        }
+      }
+      return temOrgs;
+    }
+  }
+
+  /*
+    pre view the img that user upload
+    whichPhoto value:
+      0 --> userPhoto
+      1 --> userIdPhoto
+  */
+  preViewImg(event, whichPhoto) {
+    let photoObj;
+    let photoRender;
+    if (whichPhoto == 0) {
+      photoObj = document.getElementById('userPhoto');
+      //assign photo to photoToSubmit
+      this.photoToSubmit = <File>event.target.files[0];
+      photoRender = this.photoToSubmit;
+    }
+    else {
+      photoObj = document.getElementById('userIdPhoto');
+      //assign id photo to idPhotoToSubmit
+      this.idPhotoToSubmit = <File>event.target.files[0];
+      photoRender = this.idPhotoToSubmit;
+    }
+    //important! 
+    //set src and read it 
+    let reader = new FileReader();
+    reader.onloadend = function () {
+      photoObj.setAttribute("src", this.result.toString());
+    }
+    reader.readAsDataURL(photoRender);
+  }
+
+
+  /*
+    in add and update mode
+    Branch options hide in default, when user click week name, toggle branch options,
+  */
+  toggleBranchOptions(event, i) {
+    let dropDownObj = document.getElementById(i);
+    //set [flag] attr to element, to switch between show and hide
+    event.target.attributes.flag = !event.target.attributes.flag;
+
+    if (event.target.attributes.flag == true) {
+      dropDownObj.style.display = 'block';
+    }
+    else {
+      dropDownObj.style.display = 'none';
+    }
+  }
+
+  /////////////////////////////////////////////form group/////////////////////////////////////////////////
 
   formGroupAssemble() {
     let groupObj: any;
