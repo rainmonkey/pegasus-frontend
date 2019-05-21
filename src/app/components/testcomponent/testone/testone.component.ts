55d1ff550d7a3bd1c0d12fab52e38178ef615504
Mike <info@learnspace.co.nz>
2019-05-21 17:21:28 +1200

    update

--- src/app/components/testcomponent/testone/testone.component.ts
+++ src/app/components/testcomponent/testone/testone.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-testone',
   templateUrl: './testone.component.html',
-  styleUrls: ['./testone.component.css']
+  styleUrls: ['./testone.component.css'],
+  animations: [
+    
+  ]
 })
 export class TestoneComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 
