d88d90c82f66fcb8982525c15be2b69bdcbc49e8
Mike <info@learnspace.co.nz>
2019-05-24 21:40:35 +1200

    Update

--- src/app/components/dashboard/dashboard-components/support/charting/charting.component.ts
+++ src/app/components/dashboard/dashboard-components/support/charting/charting.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-charting',
@@ -6,10 +8,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charting.component.css']
 })
 export class ChartingComponent implements OnInit {
+  chart={
+    title:'Sessions Graph',
+    subTitle:'Number of sessions in schools'
+  }
+  public chartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Pakuranga' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Epsom' }
+  ];
+  public chartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    legend:{
+      display:true,
+      position:'bottom',
+      labels:{
+        boxWidth:12,
+      }
+    },
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{}],
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        },
+      ]
+    },
+    annotation: {
+      annotations: [
+        {
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'x-axis-0',
+          value: 'March',
+          borderColor: 'orange',
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: 'orange',
+            content: 'LineAnno',
+            position: 'bottom'
+          }
+        },
+      ],
+    },
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'rgb(66, 134, 244)',
+      backgroundColor: 'rgba(86, 147, 247,0.3)',
+    },
+  ];
+
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
