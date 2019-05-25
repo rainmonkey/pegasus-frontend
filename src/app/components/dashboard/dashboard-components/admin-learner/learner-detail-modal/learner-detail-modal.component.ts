23d78b6d3345dde8aac488806ed72b85da301f63
YuyuanDing <yuyuanding0@gmail.com>
2019-05-25 14:36:54 +1200

    update

--- src/app/components/dashboard/dashboard-components/admin-learner/learner-detail-modal/learner-detail-modal.component.ts
+++ src/app/components/dashboard/dashboard-components/admin-learner/learner-detail-modal/learner-detail-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { LearnersService } from 'src/app/services/http/learners.service';
 
@@ -7,16 +7,29 @@ import { LearnersService } from 'src/app/services/http/learners.service';
   templateUrl: './learner-detail-modal.component.html',
   styleUrls: ['./learner-detail-modal.component.css']
 })
-export class LearnerDetailModalComponent implements OnInit {
+export class LearnerDetailModalComponent implements OnInit, AfterViewInit {
   @Input() command;
   @Input() whichLearner;
+  // PropNameArray:Array<any>
   public isGroupCourse: boolean = true;
   public isCustomCourse: boolean = false;
   public learnerPurpose: Array<any>;
   public howKnown: Array<any>;
-  constructor(public activeModal: NgbActiveModal,   private LearnerListService: LearnersService,) { }
+  public Purpose: Array<any>;
+  public purposeString: any;
+  PropNameArray: any;
+  otherValueList = [];
+  howKnowList = [];
+
+  howKnow: any
+  reasonList: any
+  constructor(public activeModal: NgbActiveModal, private LearnerListService: LearnersService, ) {
+
+  }
 
   ngOnInit() {
+    this.lookUpData1()
+    this.lookUpData2()
     console.log(this.whichLearner)
   }
   chooseGroupCourse() {
@@ -27,50 +40,99 @@ export class LearnerDetailModalComponent implements OnInit {
     this.isCustomCourse = true;
     this.isGroupCourse = false;
   }
-
-  getLookups(id: number) {
-    // this.registrationService.getLookups(1)
-    //   .subscribe(
-    //     data => {
-    //       console.log('teacher info', data);
-    //       this.learnerPurpose = data.Data;
-    //     },
-    //     err => {
-    //       console.log('teacher info err', err);
-    //     }
-    //   );
-    this.LearnerListService.getLookups(2)
-      .subscribe(
-        data => {
-          console.log('learner purpose', data);
-          this.learnerPurpose = data.Data;
-          for (let lP of this.learnerPurpose) {
-            lP.isChecked = false;
-          }
-        },
-        err => {
-          console.log('learner purpose err', err);
-        }
-      );
-    this.LearnerListService.getLookups(3)
-      .subscribe(
-        data => {
-          console.log('how know', data);
-          this.howKnown = data.Data;
-          this.howKnown.map((o, i) =>
-            o.isChecked = false)
-        },
-        err => {
-          console.log('how know err', err);
-        }
-      );
+  ngAfterViewInit() {
   }
 
-   /*
-    if photo not found, set default photo 
-  */
- setDefaultPhoto(event) {
-  event.target.src = '../../../../../../assets/images/shared/default-employer-profile.png';
-  return;
+  ////!!!!!!!!!!!!原本
+  // lookUpData1(){
+  //   this.LearnerListService.getLookups(2).subscribe(
+  //     (res)=>{console.log(res), this.lookUpData2(res['Data'])},
+  //     (err)=>{console.warn(err)}
+  //   )
+  // }
+  // lookUpData2(data1){
+  //   this.LearnerListService.getLookups(3).subscribe(
+  //     (res)=>{console.log(res), 
+
+  //       this.getPurposeValue(data1.concat(res['Data'])
+  //       )},
+  //     (err)=>{console.warn(err)}
+  //   )
+  // }
+
+  lookUpData1() {
+    this.LearnerListService.getLookups(2).subscribe(
+      (res) => { console.log(res), this.getPurposeValue(res.Data) },
+      (err) => { console.warn(err) }
+    )
+  }
+  lookUpData2() {
+    this.LearnerListService.getLookups(3).subscribe(
+      (res) => { console.log(res), this.getHowKnowValue(res.Data) },
+      (err) => { console.warn(err) }
+    )
+  }
+
+  getPurposeValue(displayDatas) {
+    console.log(displayDatas)
+
+    this.whichLearner.LearnerOthers.forEach(learnerOther => {
+      console.log(learnerOther)
+      if (learnerOther.OthersType == "2") {
+        displayDatas.forEach(displayData => {
+          console.log(displayData)
+          if (learnerOther.OthersValue == displayData['PropValue']) {
+            console.log(displayData)
+            this.otherValueList.push(displayData['PropName'])
+          }
+        })
+      }
+    })
+
+    // 原本
+    // getPurposeValue(displayDatas){
+    //   console.log(displayDatas)
+
+    //   this.whichLearner.LearnerOthers.forEach(learnerOther => {
+    //     console.log(learnerOther)
+    //     if(learnerOther.OthersType == "2" || learnerOther.OthersType== "3"){
+    //         displayDatas.forEach(displayData => {
+    //           console.log(displayData)
+    //           if(learnerOther.OthersValue == displayData['PropValue']){
+    //             console.log(displayData)
+    //             this.otherValueList.push(displayData['PropName'])
+    //           }
+    //         })
+    //     }
+    //   })
+    console.log(this.otherValueList)
+  }
+
+  getHowKnowValue(displayData) {
+    console.log(displayData)
+    this.whichLearner.LearnerOthers.forEach(learnerOther => {  
+      if (learnerOther.OthersType == "3") {
+        displayData.forEach(displayData => {
+          console.log(displayData)
+          if (learnerOther.OthersValue == displayData['PropValue']) {
+            console.log(displayData)
+            this.howKnowList.push(displayData['PropName'])
+          }
+        })
+      }
+      console.log(this.howKnowList)
+    })
+  }
+
+
+
+
+  /*
+   if photo not found, set default photo 
+ */
+  setDefaultPhoto(event) {
+    event.target.src = '../../../../../../assets/images/shared/default-employer-profile.png';
+    return;
+  }
 }
-}
+
