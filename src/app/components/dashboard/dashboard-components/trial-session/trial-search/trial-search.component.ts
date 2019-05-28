5129101c1774613f2bccccbd4244bd9be3d40a70
GIJoeRambo <richardnzworking@gmail.com>
2019-05-28 23:40:40 +1200

    aaa

---
+++ src/app/components/dashboard/dashboard-components/trial-session/trial-search/trial-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trial-search',
+  templateUrl: './trial-search.component.html',
+  styleUrls: ['./trial-search.component.css',
+              '../../teachers/teacher-panel/teacher-panel.component.css']
+})
+export class TrialSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
