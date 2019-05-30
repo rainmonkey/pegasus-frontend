5a9aba5e06e4de638452116aea2b0200f8ab1d4b
13911383489@139.COM <13911383489@139.COM>
2019-05-30 15:06:14 +1200

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
+  baseUrl: 'http://206.189.39.185:5000/api/',
+  photoUrl: 'http://206.189.39.185:5000/',
+  // baseUrl: 'http://192.168.178.76:5000/api/',
+  companyName: 'Able Music Studio',
+  siteName: 'Project Pegasus'
 };
 
 /*
