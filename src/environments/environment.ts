c4ca44bc663bf7fe060f54b62cf24da380c03397
13911383489@139.COM <13911383489@139.COM>
2019-05-30 15:20:49 +1200

    ee

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'http://45.76.123.59:5000/api/',
   photoUrl: 'http://gradspace.org:5000/',  
 
