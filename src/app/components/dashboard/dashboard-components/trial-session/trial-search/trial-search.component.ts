1a78fbc75b6e9d81743017639c1df6c7a1f8c3fe
GIJoeRambo <richardnzworking@gmail.com>
2019-05-30 15:31:37 +1200

    still working on trial course regis

--- src/app/components/dashboard/dashboard-components/trial-session/trial-search/trial-search.component.ts
+++ src/app/components/dashboard/dashboard-components/trial-session/trial-search/trial-search.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
               '../../teachers/teacher-panel/teacher-panel.component.css']
 })
 export class TrialSearchComponent implements OnInit {
+  public iconName:Array<string> = ['piano.svg','drum.png','guitar.png','violin.png','cello.png','voice.png','theory.png','aural.svg'];
+  public tabsCloseFlag:boolean = false;
 
   @Input() courses;
   @Input() coursesCate;
@@ -14,6 +16,33 @@ export class TrialSearchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  displayCourses(){
+    if(this.coursesCate !== undefined){
+      return this.coursesCate.slice(0,-1);
+    }
+  }
+
+  displayInstrumentsIcon(index){
+    let iconLocalSrc:string = '../../../../../../assets/images/shared/';
+    return iconLocalSrc + this.iconName[index];
+
+  }
+
+  selectTab(event,className,index){
+    let obj:any = document.getElementsByClassName(className);
+    for (let i of obj){
+      if(Number(i.id) !== index){
+        i.style.opacity = '0';
+        i.style.transition = 'opacity 0.7s linear';
+        setTimeout(()=>{
+          i.style.display = 'none';
+          this.tabsCloseFlag = true;
+        },1000)
+      }
+    }
   }
 
 }
