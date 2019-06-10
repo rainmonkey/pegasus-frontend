2fbd4a0c0d51a326167620e44b87614a1aac96f8
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-06-11 11:55:49 +1200

    update

--- src/app/components/dashboard/dashboard-components/learner-credit/learner-credit-details/learner-credit-details.component.ts
+++ src/app/components/dashboard/dashboard-components/learner-credit/learner-credit-details/learner-credit-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LearnersService } from "../../../../../services/http/learners.service"
 import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { LearnersService } from "../../../../../services/http/learners.service"
+import { NgbootstraptableService } from "../../../../../services/others/ngbootstraptable.service"
 
 
 @Component({
@@ -12,21 +13,41 @@ export class LearnerCreditDetailsComponent implements OnInit {
 
   public data: any
   public learnerId: any
+  public remainingCourseData: any
 
   constructor(private learnerService: LearnersService,
-    private activatedRouter: ActivatedRoute) { }
+    private activatedRouter: ActivatedRoute,
+    private ngTableService: NgbootstraptableService
+  ) { }
 
   ngOnInit() {
     this.activatedRouter.paramMap.subscribe((obs: ParamMap) => {
       this.learnerId = parseInt(obs.get("id"));
     })
+
+    this.getLearnerList()
+    this.getRemainingCourses()
+    
+  }
+
+  getLearnerList() {
     this.learnerService.getLearnerList().subscribe(data => {
       this.data = data["Data"]
       this.data = this.data.find(learner => learner.LearnerId == this.learnerId)
       console.log(this.data)
-    }, error => {
-      console.log(error)
-    })
+    }, error => console.log(error))
   }
+
+  getRemainingCourses() {
+    this.learnerService.getRemainingCourses(this.learnerId).subscribe(data => {
+      this.remainingCourseData = data["Data"]
+      console.log(this.remainingCourseData)
+    }, error => console.log(error)
+    )
+  }
+
+  // onSort(orderBy) {
+  //   let orderControl = this.ngTableService.sorting(this.learnerList, orderBy)
+  // }
 
 }
