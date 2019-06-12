cc5d42cb0bd0c2dc17e186f2b10c92684e752165
YuyuanDing <dyy1678032782@gmail.com>
2019-06-12 14:52:13 +1200

    fullcalendar/core/main.js

--- src/app/components/dashboard/dashboard-components/trial-session/trial-modal/trial-modal.component.ts
+++ src/app/components/dashboard/dashboard-components/trial-session/trial-modal/trial-modal.component.ts
@@ -86,7 +86,7 @@ export class TrialModalComponent implements OnInit {
   }
 
   /*
-    events handler when user select grids on calendar 
+    events handler when user select grids on calendar
   */
   selectCallBack(info) {
     let startTimestamp = Date.parse(info.startStr);
