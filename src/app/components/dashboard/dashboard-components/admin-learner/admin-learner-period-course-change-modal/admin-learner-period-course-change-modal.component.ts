2e05087f14c18bb84ad14ecd3b72b0fdce6cca50
oliver <oliverdeng1005@hotmail.com>
2019-06-13 14:51:25 +1200

    update

--- src/app/components/dashboard/dashboard-components/admin-learner/admin-learner-period-course-change-modal/admin-learner-period-course-change-modal.component.ts
+++ src/app/components/dashboard/dashboard-components/admin-learner/admin-learner-period-course-change-modal/admin-learner-period-course-change-modal.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, Validators} from '@angular/forms';
 import {LearnersService} from '../../../../../services/http/learners.service';
 import {PeriodCourseDurationChange} from '../../../../../models/PeriodCourseDurationChange';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-admin-learner-period-course-change-modal',
   templateUrl: './admin-learner-period-course-change-modal.component.html',
   styleUrls: ['./admin-learner-period-course-change-modal.component.css']
 })
 export class AdminLearnerPeriodCourseChangeModalComponent implements OnInit {
+  errorMessage;
+  IsformError = false;
   isEditSuccess = false;
   isEditFail = false;
   isloading = false;
@@ -22,21 +24,51 @@ export class AdminLearnerPeriodCourseChangeModalComponent implements OnInit {
               private service: LearnersService) { }
 
   ngOnInit() {
-    console.log(this.learner)
     this.GetOrgRoom();
     this.PeriodCourseChangeForm = this.fb.group({
-      BeginDate: [],
-      EndDate: [],
-      BeginTime: [],
-      EndTime: [],
-      reason: [],
-      instanceId: [],
-      OrgId: [],
-      RoomId: [],
-      DayOfWeek: [],
-      IsTemporary: [],
-      CourseScheduleId: []
+      BeginDate: ['', Validators.required],
+      EndDate: ['', Validators.required],
+      BeginTime: ['', Validators.required],
+      reason: ['', Validators.required],
+      instanceId: ['', Validators.required],
+      OrgId: ['', Validators.required],
+      RoomId: ['', Validators.required],
+      DayOfWeek: ['', Validators.required],
+      IsTemporary: ['', Validators.required],
+      CourseScheduleId: ['', Validators.required]
     });
+  }
+
+  get BeginDate() {
+    return this.PeriodCourseChangeForm.get('BeginDate');
+  }
+
+  get BeginTime() {
+    return this.PeriodCourseChangeForm.get('BeginTime');
+  }
+
+  get OrgId() {
+    return this.PeriodCourseChangeForm.get('OrgId');
+  }
+
+  get RoomId() {
+    return this.PeriodCourseChangeForm.get('RoomId');
+  }
+
+  get DayOfWeek() {
+    return this.PeriodCourseChangeForm.get('DayOfWeek');
+  }
+
+  get IsTemporary() {
+    return this.PeriodCourseChangeForm.get('IsTemporary');
+  }
+
+  get reason() {
+    return this.PeriodCourseChangeForm.get('reason');
+  }
+
+  get instanceId() {
+    return this.PeriodCourseChangeForm.get('instanceId');
   }
 
   GetOrgRoom = () => {
@@ -53,6 +85,12 @@ export class AdminLearnerPeriodCourseChangeModalComponent implements OnInit {
   }
 
   submit = () => {
+    if (this.PeriodCourseChangeForm.invalid) {
+      this.errorMessage = 'The form is Invalid';
+      this.IsformError = true;
+      return;
+    }
+    this.IsformError = false;
     this.isloading = true;
     this.isConfirmClick = true;
     const model = new PeriodCourseDurationChange(
@@ -64,6 +102,7 @@ export class AdminLearnerPeriodCourseChangeModalComponent implements OnInit {
       this.PeriodCourseChangeForm.value.CourseScheduleId
     )
     this.service.PeriodCourseChange(model).subscribe(res => {
+      this.isEditFail = false;
       this.isloading = false;
       this.isEditSuccess = true;
       console.log(res);
@@ -72,6 +111,11 @@ export class AdminLearnerPeriodCourseChangeModalComponent implements OnInit {
       this.isEditFail = true;
       this.isloading = false;
       console.log(err);
+      Swal.fire({
+        type: 'error',
+        title: 'Oops...',
+        text: err.error.ErrorMessage,
+      });
     });
   }
 
@@ -85,5 +129,14 @@ export class AdminLearnerPeriodCourseChangeModalComponent implements OnInit {
     this.PeriodCourseChangeForm.patchValue({
       instanceId: courseInstanceId
     });
+  }
+
+  IsTemporaryChange = () => {
+    if (this.PeriodCourseChangeForm.value.IsTemporary == 1) {
+      this.PeriodCourseChangeForm.get('EndDate').reset();
+      this.PeriodCourseChangeForm.get('EndDate').disable();
+    } else {
+      this.PeriodCourseChangeForm.get('EndDate').enable();
+    }
   }
 }
