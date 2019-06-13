669901a90f2a18cc07cd47a569616d6471fa8450
Barrick <bhu836@aucklanduni.ac.nz>
2019-06-14 10:36:39 +1200

    lllllll

--- src/app/components/dashboard/dashboard-components/admin-learner/learner-delete-course-modal/learner-delete-course-modal.component.ts
+++ src/app/components/dashboard/dashboard-components/admin-learner/learner-delete-course-modal/learner-delete-course-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { LearnerRegistrationService } from '../../../../../services/http/learner-registration.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-learner-delete-course-modal',
@@ -9,11 +11,37 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class LearnerDeleteCourseModalComponent implements OnInit {
   isGroupCourse = false;
   @Input() whichLearner;
-  constructor(public activeModal: NgbActiveModal) {}
+  groupEndDate;
+  one2OneEndDate;
+  constructor(public activeModal: NgbActiveModal, private endCourse : LearnerRegistrationService) {}
   chooseCourse(){
     this.isGroupCourse = !this.isGroupCourse;
   }
-
+  onSubmit(){
+    let id = this.whichLearner.LearnerId;
+    let fun;
+    if(this.isGroupCourse){
+      fun = this.endCourse.endGroupCourse(id, this.groupEndDate);
+    } else{
+      fun = this.endCourse.end121Course(id, this.one2OneEndDate);
+    }
+    fun.subscribe(
+      res => {
+        Swal.fire({
+          title: 'Success!',
+          text: 'Your Work Has Been Save',
+          type: 'success',
+        });
+      },
+      error => {
+        console.log(error);
+        Swal.fire({
+          title: 'Error!',
+          text: 'Sorry! '+ error.error.ErrorMessage,
+          type: 'error',
+        });
+      });
+  }
   ngOnInit() {
   }
 
