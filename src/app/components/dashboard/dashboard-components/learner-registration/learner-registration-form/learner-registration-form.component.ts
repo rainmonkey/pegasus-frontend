027872594bca3080f87cd82418e8778c5e5d063e
13911383489@139.COM <13911383489@139.COM>
2019-06-13 14:46:06 +1200

    ddd

--- src/app/components/dashboard/dashboard-components/learner-registration/learner-registration-form/learner-registration-form.component.ts
+++ src/app/components/dashboard/dashboard-components/learner-registration/learner-registration-form/learner-registration-form.component.ts
@@ -187,7 +187,7 @@ export class LearnerRegistrationFormComponent implements OnInit, DoCheck, AfterV
         learnerLevel: [this.whichLearner ? this.whichLearner.LearnerLevel : this.selectLearnerLevel, Validators.required],
         location: [this.whichLearner ? this.whichLearner.OrgId : this.orgId, Validators.required],
         levelType: [this.whichLearner ? this.whichLearner.LevelType : '0'],
-        levelTypeRadio: [this.whichLearner ? this.whichLearner.LevelType: '1'],
+        levelTypeRadio: [this.whichLearner ? this.whichLearner.LevelType: 1],
         paymentPeriod: [this.whichLearner ? this.whichLearner.PaymentPeriod : '1'],
         referrer: [this.whichLearner ? this.whichLearner.Referrer : ''],
         isUnder18: [this.whichLearner ? this.whichLearner.isUnder18 : 0],
