45d30c86a5c05e30cffce08d39f3bdf8ab7ab51e
lisa <lisababy1123@gmail.com>
2019-06-25 11:55:56 +1200

    ff

--- src/app/components/dashboard/dashboard-components/support/search-name-module/search-name-module.component.ts
+++ src/app/components/dashboard/dashboard-components/support/search-name-module/search-name-module.component.ts
@@ -93,10 +93,10 @@ export class SearchNameModuleComponent implements OnInit {
         }
         // put learners information to service waiting for other component subscribe
         this.generalRepoService.fisrtName.next(this.learners);
-        console.log(this.learners)
+        //console.log(this.learners)
         //why  this.learners.length === 1 ? but this.learners is a object
         //if (this.learners.length === 1 ){
-        this.onChangePath(this.learners.LearnerId);
+        //this.onChangePath(this.learners.LearnerId);
         //}
           console.log(this.learners)
           //why  this.learners.length === 1 ? but this.learners is a object
