edc819296483a78aafd78c1e63e802dd4d1f963b
GIJoeRambo <richardnzworking@gmail.com>
2019-06-24 15:28:00 +1200

    QAQ

--- src/app/components/dashboard/general/messager/messager-recently/messager-recently.component.ts
+++ src/app/components/dashboard/general/messager/messager-recently/messager-recently.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-messager-recently',
   templateUrl: './messager-recently.component.html',
-  styleUrls: ['./messager-recently.component.css']
+  styleUrls: ['./messager-recently.component.css',
+              '../../../dashboard-components/teachers/teacher-panel/teacher-panel.component.css']
 })
 export class MessagerRecentlyComponent implements OnInit {
 
