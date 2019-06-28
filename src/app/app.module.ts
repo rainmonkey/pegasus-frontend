c8f9929c0207f8577df3c82b4c89bd67293c229c
YuyuanDing <dyy1678032782@gmail.com>
2019-06-28 15:00:15 +1200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -115,7 +115,7 @@ import { StaffModalFormComponent } from './components/dashboard/dashboard-compon
 import { InventoryDetailModalComponent } from './components/dashboard/dashboard-components/inventory/inventory-detail-modal/inventory-detail-modal.component';
 import { InventoryReceiptModalComponent } from './components/dashboard/dashboard-components/inventory/inventory-receipt-modal/inventory-receipt-modal.component';
 import { HolidayCalendarComponent } from './components/dashboard/dashboard-components/admin-holidays/holiday-calendar/holiday-calendar.component';
-import { AddHolidaysModalComponent } from './components/dashboard/dashboard-components/admin-holidays/add-holidays-modal/add-holidays-modal.component';
+
 import { DeleteHolidayComponent } from './components/dashboard/dashboard-components/admin-holidays/delete-holiday/delete-holiday.component';
 import { PickerModule } from '@ctrl/ngx-emoji-mart';
 import { EmojiModule } from '@ctrl/ngx-emoji-mart/ngx-emoji';
@@ -236,7 +236,6 @@ import { SelectHolidaysModalComponent } from './components/dashboard/dashboard-c
     InventoryDetailModalComponent,
     InventoryReceiptModalComponent,
     HolidayCalendarComponent,
-    AddHolidaysModalComponent,
     DeleteHolidayComponent,
     MessagerIconComponent,
     MessagerModalComponent,
@@ -304,7 +303,6 @@ import { SelectHolidaysModalComponent } from './components/dashboard/dashboard-c
     StaffEditModalComponent,
     InventoryDetailModalComponent,
     InventoryReceiptModalComponent,
-    AddHolidaysModalComponent,
     DeleteHolidayComponent,
     AmendmentHistoryModalComponent,
     SelectHolidaysModalComponent
--- src/app/components/dashboard/dashboard-components/admin-holidays/holiday-calendar/holiday-calendar.component.ts
+++ src/app/components/dashboard/dashboard-components/admin-holidays/holiday-calendar/holiday-calendar.component.ts
@@ -6,7 +6,6 @@ import { OptionsInput } from '@fullcalendar/core';
 import { HolidaysService } from 'src/app/services/http/holidays.service';
 import { Calendar } from '@fullcalendar/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { AddHolidaysModalComponent } from '../add-holidays-modal/add-holidays-modal.component';
 import { View } from '@fullcalendar/core';
 import { DeleteHolidayComponent } from '../delete-holiday/delete-holiday.component';
 import { preserveWhitespacesDefault } from '@angular/compiler';
@@ -67,10 +66,6 @@ export class HolidayCalendarComponent implements OnInit {
       select: function (info) {
         that.test(info)
       },
-      // dateClick: function (info) {
-      //   console.log(info)
-      //   that.addAndEdit(info)
-      // },
       eventClick: (info) => {
         // console.log(info)
         that.delete(info)
@@ -80,21 +75,6 @@ export class HolidayCalendarComponent implements OnInit {
   }
 
   test(info) {
-  //   this.selectDate = []
-  //   console.log(info)
-  //   let beginDate = info.start
-  //   const endDate = (info.end).setDate((info.end).getDate()-1)
-  // //  while (moment(beginDate).isBefore(endDate))
-  // for (beginDate; moment(beginDate).isBefore(endDate)||moment(beginDate).isSame(endDate);) {
-  //   console.log(beginDate)
-  //   this.selectDate.push(beginDate)
-  //   beginDate.setDate(beginDate.getDate() + 1)
-
-
-  //   }
-
-  //   console.log(this.selectDate)
-  //   return
   const modalRef = this.modalService.open(SelectHolidaysModalComponent)
     let that = this;
     modalRef.componentInstance.date = info;
@@ -123,28 +103,6 @@ export class HolidayCalendarComponent implements OnInit {
       this.holidayArray.push({ 'id': i.HolidayId, "title": i.HolidayName, "date": i.HolidayDate })
     }
     return this.holidayArray;
-  }
-
-  addAndEdit(info) {
-    const modalRef = this.modalService.open(AddHolidaysModalComponent)
-    let that = this;
-    modalRef.componentInstance.date = info;
-    modalRef.componentInstance.refreshFlag.subscribe(
-      (res) => {
-        modalRef.result.then(
-          function () {
-            // console.log(res)
-            if (res == true) {
-              that.fullcalendar.calendar.removeAllEvents();
-              that.getExitHoliday()
-              that.initFullCalendar(this)
-            }
-          },
-          function () {
-            return;
-          })
-      }
-    )
   }
 
   delete(info) {
