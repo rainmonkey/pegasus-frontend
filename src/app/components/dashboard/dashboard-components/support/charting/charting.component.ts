ea04147a5244eb0ca50ff7e743554b9f351dba7d
Barrick <bhu836@aucklanduni.ac.nz>
2019-07-02 11:09:59 +1200

    curious

--- src/app/components/dashboard/dashboard-components/support/charting/charting.component.ts
+++ src/app/components/dashboard/dashboard-components/support/charting/charting.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, BaseChartDirective, Label } from 'ng2-charts';
 import { SessionsService } from 'src/app/services/http/sessions.service';
+import { DashboardService } from '../../../../../services/http/dashboard.service';
 
 @Component({
   selector: 'app-charting',
@@ -13,16 +14,38 @@ export class ChartingComponent implements OnInit {
     title:'Sessions Graph',
     subTitle:'Number of sessions in schools'
   }
+  assignChartData = [];
   public chartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Pakuranga' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Epsom' }
+    {
+      label: "lessons For Recent 14 Days",
+      fill: true,
+      lineTension: 0,
+      backgroundColor: "transparent",
+      borderColor: '#f15765',
+      pointBorderColor: '#da4c59',
+      pointHoverBackgroundColor: '#da4c59',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      borderWidth: 1,
+      pointBackgroundColor: "#fff",
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBorderColor: "#fff",
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 0,
+      data: [],
+      spanGaps: false
+  }
   ];
-  public chartLabels: Label[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+  public chartLabels: Label[] = [];
   public lineChartOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
     legend:{
       display:true,
-      position:'bottom',
+      position:'top',
       labels:{
         boxWidth:12,
       }
@@ -58,21 +81,36 @@ export class ChartingComponent implements OnInit {
   };
   public lineChartColors: Color[] = [
     {
-      borderColor: 'rgb(66, 134, 244)',
-      backgroundColor: 'rgba(86, 147, 247,0.3)',
+
     },
   ];
 
   public lineChartLegend = true;
   public lineChartType = 'line';
   public lineChartPlugins = [];
+  // recent course data
+  recentCourse;
 
   constructor(
-    public sessionService: SessionsService
+    public sessionService: SessionsService,
+    private dashBoardService: DashboardService
   ) { }
-
-
+  getStatistic(){
+    let brunch = localStorage.getItem('OrgId').slice(1,-1);
+    this.dashBoardService.getStatistic(brunch).subscribe(
+      res=>{
+        console.log(res)
+        this.recentCourse = res.Data.lessonsForRecent14Days;
+        this.chartLabels = Object.keys(this.recentCourse);
+        this.assignChartData = Object.values(this.recentCourse);
+        this.chartData[0].data = this.assignChartData;
+        console.log(this.assignChartData)
+        console.log(this.recentCourse)
+      }
+    )
+  }
   ngOnInit() {
+    this.getStatistic();
   //   let beginDate = '2019-04-29'
   //   let endDate = '2019-05-10'
   //   this.sessionService.getReceptionistLessonBetweenDate('2019-04-29', '2019-05-10').subscribe(
