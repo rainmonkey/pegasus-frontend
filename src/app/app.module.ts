b9a2544c30400da590f4c74101a42e88578851fb
13911383489@139.COM <13911383489@139.COM>
2019-07-10 11:41:33 +1200

    add shared learner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -78,6 +78,7 @@ import { DashboardPanelComponent } from './components/dashboard/general/dashboar
 import { HeaderbarComponent } from './components/dashboard/general/headerbar/headerbar.component';
 import { SidebarComponent } from './components/dashboard/general/sidebar/sidebar.component';
 import { TestoneComponent } from './components/testcomponent/testone/testone.component';
+import { LearnerItemComponent } from './shared/components/learner-item/learner-item.component';
 import { ColumnTitleFormatPipe } from './shared/pipes/column-title-format.pipe';
 // Guards
 // Services
@@ -188,7 +189,7 @@ import { ConflictCheckComponent } from './components/dashboard/dashboard-compone
     CourseDetailModalComponent,
     CourseDeleteModalComponent,
     TestoneComponent,
-
+    LearnerItemComponent,
     LearnerDeleteModalComponent,
     LearnerDetailModalComponent,
     LearnerEditModalComponent,
@@ -284,6 +285,7 @@ import { ConflictCheckComponent } from './components/dashboard/dashboard-compone
     NgbActiveModal,
   ],
   entryComponents: [
+    AdminLearnerProfileComponent,
     TeacherDeleteModalComponent,
     TeacherDetailModalComponent,
     TeacherUpdateModalComponent,
@@ -293,6 +295,7 @@ import { ConflictCheckComponent } from './components/dashboard/dashboard-compone
     CourseDetailModalComponent,
     SessionDetailEditModalComponent,
     CourseClassDetailModalComponent,
+    LearnerItemComponent,
     LearnerDeleteModalComponent,
     LearnerDetailModalComponent,
     LearnerEditModalComponent,
