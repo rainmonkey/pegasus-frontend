aafb95b67923120de560571e860d4646209b3776
13911383489@139.COM <13911383489@139.COM>
2019-07-15 12:34:06 +1200

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule ,ErrorHandler} from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
 // Dependencies
@@ -103,6 +103,7 @@ import { RemindPanelComponent } from './components/dashboard/dashboard-component
 
 import { StaffListComponent } from './components/dashboard/dashboard-components/admin-staff/Staff-list/Staff-list.component';
 
+import {CustomErrorHandler} from './services/errorhandler/CustomErrorHandler'
 import { SimplifyOrgPipe } from './shared/pipes/simplify-org.pipe';
 import { LearnerCreditPanelComponent } from './components/dashboard/dashboard-components/learner-credit/learner-credit-panel/learner-credit-panel.component';
 import { LearnerCreditDetailsComponent } from './components/dashboard/dashboard-components/learner-credit/learner-credit-details/learner-credit-details.component';
@@ -211,7 +212,6 @@ import { ConflictCheckComponent } from './components/dashboard/dashboard-compone
     LearnerDeleteCourseModalComponent,
     AmendmentHistoryModalComponent,
     IsTemporaryPipe,
-
     SessionCancelModalComponent,
     SessionTutorReportModalComponent,
     SessionCompletedModalComponent,
@@ -300,6 +300,10 @@ import { ConflictCheckComponent } from './components/dashboard/dashboard-compone
     DatePipe,
     MondayDateInWeekByDatePipe,
     NgbActiveModal,
+    {
+      provide: ErrorHandler,
+      useClass: CustomErrorHandler,
+    },
   ],
   entryComponents: [
     AdminLearnerProfileComponent,
