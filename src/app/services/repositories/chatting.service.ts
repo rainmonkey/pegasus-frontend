5a20b9993d8fa0434e4bfe196cdc991145e85c61
GIJoeRambo <richardnzworking@gmail.com>
2019-07-23 00:27:59 +1200

    reconnect

--- src/app/services/repositories/chatting.service.ts
+++ src/app/services/repositories/chatting.service.ts
@@ -14,6 +14,7 @@ export class ChattingService {
   public hubConnection: signalR.HubConnection;
   public baseUrlForChatting: any = environment.baseUrlForChatting;
   public baseUrl: any = environment.baseUrl;
+  public reconnectCounter:number = 0;
 
   constructor(private http: HttpClient,
     private messagerService: MessagerService) { }
@@ -23,14 +24,40 @@ export class ChattingService {
     this.hubConnection = new signalR.HubConnectionBuilder()
       .withUrl(this.baseUrlForChatting + 'chat?userId=' + userId)
       .build();
-
-    return this.hubConnection
+    console.log('1111')
+    this.hubConnection
       .start()
       .then(() => {
         console.log('connection started');
+        this.reconnectCounter = 0;
         this.listenMessage();
       })
-      .catch(err => console.log(err));
+      //当connect连接出现错误(连接失败)
+      .catch(err => {
+        console.log(err);
+        console.log('???')
+        this.reconnect(userId);
+      });
+
+    this.hubConnection
+    //当连接成功后 断线(连接断开)
+      .onclose(() =>{
+        console.log("!!!")
+        this.reconnect(userId);
+      })
+  }
+
+  reconnect(userId){
+    //重连10次
+    if(this.reconnectCounter > 10){
+      return;
+    }
+    let reconnectTimeInterval = this.reconnectCounter === 0? 0 : 5000;
+    console.log(reconnectTimeInterval);
+    setTimeout(()=>{
+      this.startConnection(userId)
+    },reconnectTimeInterval)
+    this.reconnectCounter ++;
   }
 
   //send message
@@ -38,9 +65,10 @@ export class ChattingService {
     //return from(this.hubConnection.invoke('SendMessageOneToOne', messageObj));
     return forkJoin(
       from(this.hubConnection.invoke('SendMessageOneToOne', messageObj)),
-      this.http.post(this.baseUrl + 'chat', messageObj).pipe(
-        retry(2)
-      )
+      //放后台吧这个东西
+      // this.http.post(this.baseUrl + 'chat', messageObj).pipe(
+      //   retry(2)
+      // )
     )
 
 
@@ -70,7 +98,7 @@ export class ChattingService {
     this.hubConnection.on('SendMessageOneToOne',
       (id, message, messageTime) => {
         //接收信息处理
-        this.messagerService.saveChattingHistory({ subscriberId: id, message: message, leftOrRight: 'left', createTime: messageTime,isError:false,isResend:false })
+        this.messagerService.saveChattingHistory({ subscriberId: id, message: message, leftOrRight: 'left', createTime: messageTime, isError: false, isResend: false })
       }),
       (err) => {
         console.log(err)
