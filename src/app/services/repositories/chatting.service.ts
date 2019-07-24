8d6804231e67621e2d9ac18e7c8c597ce5bf002b
13911383489@139.COM <13911383489@139.COM>
2019-07-24 16:57:39 +1200

    fix error

--- src/app/services/repositories/chatting.service.ts
+++ src/app/services/repositories/chatting.service.ts
@@ -84,7 +84,7 @@ export class ChattingService {
   sendMessage(messageObj) {
     //return from(this.hubConnection.invoke('SendMessageOneToOne', messageObj));
     
-      from(this.hubConnection.invoke('SendMessageOneToOne', messageObj))
+      return from(this.hubConnection.invoke('SendMessageOneToOne', messageObj))
       //放后台吧这个东西
       // this.http.post(this.baseUrl + 'chat', messageObj).pipe(
       //   retry(2)
