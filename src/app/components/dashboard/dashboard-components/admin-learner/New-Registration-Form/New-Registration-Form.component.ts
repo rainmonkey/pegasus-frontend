f1985512a2cce48e94427c16766c84d39fc28eb0
YuyuanDing <dyy1678032782@gmail.com>
2019-07-31 17:53:03 +1200

    1

--- src/app/components/dashboard/dashboard-components/admin-learner/New-Registration-Form/New-Registration-Form.component.ts
+++ src/app/components/dashboard/dashboard-components/admin-learner/New-Registration-Form/New-Registration-Form.component.ts
@@ -19,60 +19,64 @@ export class NewRegistrationFormComponent implements OnInit {
 
   ngOnInit() {
     this.registrationForm = this.fb.group(this.formBuild());
+
+
   }
 
-  formBuild(){
+  formBuild() {
     let groupObj: any;
-    if(this.command == 1 ){
-      groupObj={
-        FirstName:[null, Validators.required],
-        MiddleName:[null],
+    if (this.command == 1) {
+      groupObj = {
+        FirstName: [null, Validators.required],
+        MiddleName: [null],
         LastName: [null, Validators.required],
         Gender: [null, Validators.required],
-        dob: [null, Validators.required],
-        EnrollDate: [null, Validators.required],
-        ContactNum:[null, Validators.required],
-        Email:[null, [Validators.required,Validators.email]],
-        Address:[null],
-        OrgId:[null, Validators.required],
-        LearnerLevel:[null, Validators.required],
-        LevelType:[null, Validators.required],
-        IsUnder18:[null, Validators.required],
-        PaymentPeriod:[null, Validators.required],
-        Referrer:[null],
-        Comment:[null],
+        dob: [null],
+        EnrollDate: [null],
+        ContactNum: [null],
+        Email: [null, [Validators.required, Validators.email]],
+        Address: [null],
+        OrgId: [null, Validators.required],
+        LearnerLevel: [null, Validators.required],
+        LevelType: [null],
+        IsUnder18: [null],
+        PaymentPeriod: [null],
+        Referrer: [null],
+        Comment: [null],
 
         // photo
-        photo:[null],
-        grade:[null],
-        Agreement:[null],
-        OtherFile:[null],
+        photo: [null],
+        grade: [null],
+        Agreement: [null],
+        OtherFile: [null],
       }
     }
-    else if(this.command==2){
-      groupObj={
-        FirstName:[{value:this.whichLearner.FirstName}, Validators.required],
-        MiddleName:[{value:this.whichLearner.MiddleName}],
-        LastName: [{value:this.whichLearner.LastName}, Validators.required],
-        Gender: [{value:this.whichLearner.Gender}, Validators.required],
-        dob: [{value:this.whichLearner.Dob}, Validators.required],
-        EnrollDate: [{value:this.whichLearner.EnrollDate}, Validators.required],
-        ContactNum:[{value:this.whichLearner.ContactNum}, Validators.required],
-        Email:[{value:this.whichLearner.Email}, [Validators.required,Validators.email]],
-        Address:[{value:this.whichLearner.Address}],
-        OrgId:[{value:this.whichLearner.OrgId}, Validators.required],
-        LearnerLevel:[{value:this.whichLearner.LearnerLevel}, Validators.required],
-        LevelType:[{value:this.whichLearner.LevelType}, Validators.required],
-        IsUnder18:[{value:this.whichLearner.IsUnder18}, Validators.required],
-        PaymentPeriod:[{value:this.whichLearner.PaymentPeriod}, Validators.required],
-        Referrer:[{value:this.whichLearner.Referrer}],
-        Comment:[{value:this.whichLearner.Comment}],
+    else {
+      console.log(this.whichLearner.FirstName)
+      console.log(typeof(this.whichLearner.FirstName))
+      groupObj = {
+        FirstName: [this.whichLearner.FirstName , Validators.required],
+        MiddleName: [ this.whichLearner.MiddleName ? this.whichLearner.MiddleName : ' ' ],
+        LastName: [this.whichLearner.LastName, Validators.required],
+        Gender: [ this.whichLearner.Gender, Validators.required],
+        dob: [this.whichLearner.Dob ],
+        EnrollDate: [ this.whichLearner.EnrollDate ],
+        ContactNum: [this.whichLearner.ContactNum ],
+        Email: [this.whichLearner.Email,[Validators.required, Validators.email]],
+        Address: [this.whichLearner.Address ],
+        OrgId: [this.whichLearner.OrgId , Validators.required],
+        LearnerLevel: [ this.whichLearner.LearnerLevel , Validators.required],
+        LevelType: [this.whichLearner.LevelType ],
+        IsUnder18: [ this.whichLearner.IsUnder18 ],
+        PaymentPeriod: [this.whichLearner.PaymentPeriod ],
+        Referrer: [this.whichLearner.Referrer],
+        Comment: [this.whichLearner.Comment ],
 
         // photo
-        photo:[null],
-        grade:[null],
-        Agreement:[null],
-        OtherFile:[null],
+        photo: [null],
+        grade: [null],
+        Agreement: [null],
+        OtherFile: [null],
       }
     }
     return groupObj
