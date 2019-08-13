8157c113df799e4625039fcb87fe66029fd3f31d
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-08-13 15:32:34 +1200

    update

--- src/app/components/dashboard/dashboard-components/trial-course/trial-calendar/trial-calendar.component.ts
+++ src/app/components/dashboard/dashboard-components/trial-course/trial-calendar/trial-calendar.component.ts
@@ -1,47 +1,73 @@
-import { TrialConfirmationComponent } from './../trial-confirmation/trial-confirmation.component';
-import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
-import { OptionsInput } from '@fullcalendar/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import { TrialCoursesService } from 'src/app/services/http/trial-courses.service';
-import { forkJoin, Observable } from 'rxjs';
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { OptionsInput } from "@fullcalendar/core";
+import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { TrialCoursesService } from "src/app/services/http/trial-courses.service";
+import { TeachersService } from "src/app/services/http/teachers.service";
+import { TrialConfirmationComponent } from "./../trial-confirmation/trial-confirmation.component";
+import { forkJoin, Observable } from "rxjs";
 
 @Component({
-  selector: 'app-trial-calendar',
-  templateUrl: './trial-calendar.component.html',
-  styleUrls: ['./trial-calendar.component.css'],
+  selector: "app-trial-calendar",
+  templateUrl: "./trial-calendar.component.html",
+  styleUrls: ["./trial-calendar.component.css"],
   encapsulation: ViewEncapsulation.None
 })
 export class TrialCalendarComponent implements OnInit {
   public options: OptionsInput;
   public timeSlots: any;
-  @Input() teacher: Object;
+  @Input() teacher: object;
   @Input() orgName: string;
   @Input() orgId: number;
   @Input() courseCategoryName: string;
   @Input() CourseCategoryId: number;
 
+  selectMode = false;
+  @Input() teacherId: number;
+  @Input() duration: number;
+  @Output() userSelectedTime = new EventEmitter();
+  @Input() durationType: number;
+
   constructor(
     private modalService: NgbModal,
     private activeModal: NgbActiveModal,
-    private trialService: TrialCoursesService
-  ) { }
+    private trialService: TrialCoursesService,
+    private teacherService: TeachersService
+  ) {}
 
   ngOnInit() {
-    this.getDataFromServer().subscribe(
-      (res) => {
-    
-        let coursesSlots = res[0]['Data'].map(
-          (val)=>{
-            val.title = val.roomName;
-            return val;
-          }
-        )
-        this.timeSlots = this.calculateAvaliableTimeSlots(res[1]['Data']);
-        this.initializeFullCalendar(coursesSlots, this.timeSlots);
+    if (this.duration || this.durationType) {
+      this.selectMode = true;
+    }
+    if (this.durationType) {
+      if (this.durationType === 1) {
+        this.duration = 180000;
+      } else if (this.durationType === 2) {
+        this.duration = 270000;
+      } else if (this.durationType === 3) {
+        this.duration = 360000;
       }
-    );
+    }
+    console.log(this.duration, this.durationType);
+    this.getDataFromServer().subscribe(res => {
+      if (this.selectMode) {
+        this.teacher = res[2]["Data"];
+      }
+      const coursesSlots = res[0]["Data"].map(val => {
+        val.title = val.roomName;
+        return val;
+      });
+      this.timeSlots = this.calculateAvaliableTimeSlots(res[1]["Data"]);
+      this.initializeFullCalendar(coursesSlots, this.timeSlots);
+    });
   }
 
   /**
@@ -49,9 +75,25 @@ export class TrialCalendarComponent implements OnInit {
    * @returns {Observable} observable of data (coursesOfATeacher, terms of a year)
    */
   getDataFromServer(): Observable<Array<object>> {
-    let apiRequests: Array<any> = [];
-    apiRequests.push(this.trialService.getTeacherCoursesbyId(this.teacher['TeacherId']));
-    apiRequests.push(this.trialService.getTermsDuration());
+    const apiRequests: Array<any> = [];
+    if (this.selectMode) {
+      apiRequests.push(
+        this.trialService.getTeacherCoursesbyId(
+          this.teacherId || this.teacher["TeacherId"]
+        )
+      );
+      apiRequests.push(this.trialService.getTermsDuration());
+      apiRequests.push(
+        this.teacherService.getTeacherInfo(
+          this.teacherId || this.teacher["TeacherId"]
+        )
+      );
+    } else {
+      apiRequests.push(
+        this.trialService.getTeacherCoursesbyId(this.teacher["TeacherId"])
+      );
+      apiRequests.push(this.trialService.getTermsDuration());
+    }
     return forkJoin(apiRequests);
   }
 
@@ -65,40 +107,46 @@ export class TrialCalendarComponent implements OnInit {
    * @returns {Array<object>} list of avaliable time slots after calculation
    */
   calculateAvaliableTimeSlots(terms: Array<object>) {
-
     /**@property {Array<number>} dayOfWeek - teacher's avaliable day of one week */
-    let dayOfWeek: Array<number> = this.getDayOfWeek(this.teacher);
+    const dayOfWeek: Array<number> = this.getDayOfWeek(this.teacher);
     /**@property {Array<object>} avaliableTermDuration - avaliable term duration*/
-    let avaliableTermDuration: Array<object> = this.getAvaliableTermDuration(terms);
+    const avaliableTermDuration: Array<object> = this.getAvaliableTermDuration(
+      terms
+    );
     /**@property {Array<object>} avaliableTimeSlots - a list saved avaliable time slots and returned at the end of function */
-    let avaliableTimeSlots: Array<object> = [];
+    const avaliableTimeSlots: Array<object> = [];
     const millisecOfOneDay: number = 86400000;
 
     /**To estimate whether a day is avaliable*/
-    avaliableTermDuration.map(
-      (val) => {
-        let endDateTimeStamp = new Date(val['EndDate']).getTime();
-        //start date is from current day(days before current day are all unavaliable)
-        let startDateTimeStamp = new Date().getTime();
-        //iterator of term's duration times
-        for (let i = startDateTimeStamp; i < endDateTimeStamp; i += millisecOfOneDay) {
-          let date = new Date(i);
+    avaliableTermDuration.map(val => {
+      const endDateTimeStamp = new Date(val["EndDate"]).getTime();
+      //start date is from current day(days before current day are all unavaliable)
+      const startDateTimeStamp = new Date().getTime();
+      //iterator of term's duration times
+      for (
+        let i = startDateTimeStamp;
+        i < endDateTimeStamp;
+        i += millisecOfOneDay
+      ) {
+        const date = new Date(i);
 
-          let weekDay = date.getDay() == 0 ? 7 : date.getDay();
-          let year = date.getFullYear();
-          let month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
-          let day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+        const weekDay = date.getDay() == 0 ? 7 : date.getDay();
+        const year = date.getFullYear();
+        const month =
+          date.getMonth() + 1 < 10
+            ? "0" + (date.getMonth() + 1)
+            : date.getMonth() + 1;
+        const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
 
-          if (dayOfWeek.includes(weekDay)) {
-            avaliableTimeSlots.push({
-              start: `${year}-${month}-${day}T00:00:00`,
-              end: `${year}-${month}-${day}T23:59:59`,
-              rendering: 'background'
-            });
-          }
+        if (dayOfWeek.includes(weekDay)) {
+          avaliableTimeSlots.push({
+            start: `${year}-${month}-${day}T00:00:00`,
+            end: `${year}-${month}-${day}T23:59:59`,
+            rendering: "background"
+          });
         }
       }
-    )
+    });
     return avaliableTimeSlots;
   }
 
@@ -108,12 +156,10 @@ export class TrialCalendarComponent implements OnInit {
    * @returns {Array<number>} array of avaliable days of one week
    */
   getDayOfWeek(teacher: object) {
-    let array: Array<number> = [];
-    teacher['AvailableDays'].map(
-      (val) => {
-        array.push(val.DayOfWeek)
-      }
-    )
+    const array: Array<number> = [];
+    teacher["AvailableDays"].map(val => {
+      array.push(val.DayOfWeek);
+    });
     return array;
   }
 
@@ -123,16 +169,14 @@ export class TrialCalendarComponent implements OnInit {
    */
   getAvaliableTermDuration(terms) {
     /**@property {number} timeStampOfToday - stamp of today */
-    let timeStampOfToday: number = new Date().getTime();
-    let avaliableTermDuration: Array<object> = terms.filter(
-      (val) => {
-        /**@property {number} - end date time stamp of each term*/
-        let timeStamp: number = new Date(val['EndDate']).getTime();
-        if (timeStamp > timeStampOfToday) {
-          return val;
-        }
+    const timeStampOfToday: number = new Date().getTime();
+    const avaliableTermDuration: Array<object> = terms.filter(val => {
+      /**@property {number} - end date time stamp of each term*/
+      const timeStamp: number = new Date(val["EndDate"]).getTime();
+      if (timeStamp > timeStampOfToday) {
+        return val;
       }
-    )
+    });
     return avaliableTermDuration;
   }
 
@@ -141,51 +185,94 @@ export class TrialCalendarComponent implements OnInit {
    * @param coursesTimeSlots - time slots of existed sessions.
    * @param avaliableDaySlots - time slots of teacher's avaliable days in one or more terms.
    */
-  initializeFullCalendar(coursesTimeSlots: Array<object>, avaliableDaySlots: Array<any>) {
-    let that = this;
+  initializeFullCalendar(
+    coursesTimeSlots: Array<object>,
+    avaliableDaySlots: Array<any>
+  ) {
+    const that = this;
     this.options = {
       allDaySlot: false,
       height: 700,
-      selectable: true,
-      minTime: '09:00',
-      maxTime: '20:00',
-      slotDuration: '00:15',
-      eventSources: [coursesTimeSlots,
-        avaliableDaySlots],
-      eventBackgroundColor: 'lightblue',
+      selectable: this.selectMode ? false : true,
+      minTime: "09:00",
+      maxTime: "20:00",
+      slotDuration: "00:15",
+      eventSources: [coursesTimeSlots, avaliableDaySlots],
+      eventBackgroundColor: "lightblue",
       selectMirror: true,
-      selectOverlap: function (event) {
-        return event.rendering === 'background';
+      selectOverlap(event) {
+        return event.rendering === "background";
       },
       selectConstraint: avaliableDaySlots,
       select(info) {
         that.selectSlot(info);
       },
       header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'timeGridWeek'
+        left: "prev,next today",
+        center: "title",
+        right: "timeGridWeek"
+      },
+      dateClick: info => {
+        const occupiedStartTimestamps = coursesTimeSlots.map(
+          el => +new Date(el["start"])
+        );
+        let diff;
+        for (let i = 0; i < occupiedStartTimestamps.length; i++) {
+          diff = occupiedStartTimestamps[i] - +info.date;
+          if (diff < 3600000 && diff > 0 && diff < this.duration) {
+            break;
+          }
+          diff = 0;
+        }
+        if (diff) {
+          alert(
+            `Your selected time is ${info.dateStr
+              .split("+")[0]
+              .replace("T", " ")} to ${new Date(
+              +info.date +
+                this.duration -
+                new Date().getTimezoneOffset() * 60000
+            )
+              .toISOString()
+              .replace("T", " ")
+              .slice(
+                0,
+                length - 5
+              )}, which has been occupied by other course, please select another time.`
+          );
+        } else {
+          this.userSelectedTime.emit(info.dateStr.split("+")[0]);
+          this.activeModal.close();
+        }
       },
       plugins: [timeGridPlugin, interactionPlugin]
-    }
+    };
   }
 
   /**
    * Call back function of full calendar select.
-   * If selected time slots >= 30 min and <= 60 min, popup confirmation modal.   
+   * If selected time slots >= 30 min and <= 60 min, popup confirmation modal.
    * @param info - infomation of select event
    */
   selectSlot(info) {
     const milliSecIn30Min = 1800000;
     const milliSecIn60Min = 3600000;
-    let startStr = info.startStr;
-    let startTimeStamp = new Date(startStr).getTime();
-    let endStr = info.endStr;
-    let endTimeStamp = new Date(endStr).getTime();
-    let differTimeStamp = endTimeStamp - startTimeStamp;
+    const startStr = info.startStr;
+    const startTimeStamp = new Date(startStr).getTime();
+    const endStr = info.endStr;
+    const endTimeStamp = new Date(endStr).getTime();
+    const differTimeStamp = endTimeStamp - startTimeStamp;
 
-    if (differTimeStamp >= milliSecIn30Min && differTimeStamp <= milliSecIn60Min) {
-      this.popUpConfirmationModal(startTimeStamp, endTimeStamp, startStr, endStr);
+    if (
+      differTimeStamp >= milliSecIn30Min &&
+      differTimeStamp <= milliSecIn60Min
+    ) {
+      this.popUpConfirmationModal(
+        startTimeStamp,
+        endTimeStamp,
+        startStr,
+        endStr
+      );
     }
   }
 
@@ -194,13 +281,22 @@ export class TrialCalendarComponent implements OnInit {
    * @param courseStartTime - start time stamp
    * @param courseEndTime - end time stamp
    * @param startStr - start time string
-   * @param endStr - end time string 
+   * @param endStr - end time string
    */
-  popUpConfirmationModal(courseStartTime: number, courseEndTime: number, startStr: string, endStr: string) {
-    const modalRef = this.modalService.open(TrialConfirmationComponent, { size: 'lg', backdrop: 'static', keyboard: false });
+  popUpConfirmationModal(
+    courseStartTime: number,
+    courseEndTime: number,
+    startStr: string,
+    endStr: string
+  ) {
+    const modalRef = this.modalService.open(TrialConfirmationComponent, {
+      size: "lg",
+      backdrop: "static",
+      keyboard: false
+    });
     modalRef.componentInstance.confirmationData = {
-      teacherId: this.teacher['TeacherId'],
-      teacherName: this.teacher['FirstName'] + '  ' + this.teacher['LastName'],
+      teacherId: this.teacher["TeacherId"],
+      teacherName: this.teacher["FirstName"] + "  " + this.teacher["LastName"],
       startTimeStamp: courseStartTime,
       endTimeStamp: courseEndTime,
       orgId: this.orgId,
@@ -208,13 +304,11 @@ export class TrialCalendarComponent implements OnInit {
       cateName: this.courseCategoryName,
       cateId: this.CourseCategoryId,
       teacher: this.teacher,
-      startStr: startStr,
-      endStr: endStr
-    }
-    modalRef.componentInstance.isClosed.subscribe(
-      (res) => {
-        this.activeModal.close('Close click');
-      }
-    )
+      startStr,
+      endStr
+    };
+    modalRef.componentInstance.isClosed.subscribe(res => {
+      this.activeModal.close("Close click");
+    });
   }
 }
--- src/app/components/dashboard/dashboard-components/trial-course/trial-filter/trial-filter.component.ts
+++ src/app/components/dashboard/dashboard-components/trial-course/trial-filter/trial-filter.component.ts
@@ -1,21 +1,31 @@
-import { CoursesService } from 'src/app/services/http/courses.service';
-import { Component, OnInit } from '@angular/core';
-import { TeachersService } from 'src/app/services/http/teachers.service';
-import { forkJoin } from 'rxjs';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { TrialCalendarComponent } from '../trial-calendar/trial-calendar.component';
+import { map } from "rxjs/operators";
+import { CoursesService } from "src/app/services/http/courses.service";
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { TeachersService } from "src/app/services/http/teachers.service";
+import { forkJoin } from "rxjs";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { TrialCalendarComponent } from "../trial-calendar/trial-calendar.component";
+import { TransactionService } from "src/app/services/http/transaction.service";
 
 @Component({
-  selector: 'app-trial-filter',
-  templateUrl: './trial-filter.component.html',
-  styleUrls: ['./trial-filter.component.css']
+  selector: "app-trial-filter",
+  templateUrl: "./trial-filter.component.html",
+  styleUrls: ["./trial-filter.component.css"]
 })
 export class TrialFilterComponent implements OnInit {
-  public filterLabel: Array<string> = ['Categories', 'Orgnizations', 'DayOfWeek'];
+  @Input() arrangeFlag: boolean;
+  arrangeCourseDetails: any;
+
+  public filterLabel: Array<string> = [
+    "Categories",
+    "Orgnizations",
+    "DayOfWeek"
+  ];
   public orgIdFilter: number;
-  public orgName:string;
+  public orgName: string;
   public cateIdFilter: number;
-  public cateName:string;
+  public cateName: string;
 
   /**@property {Array<string>} filterString -  A list stored the filter tags that selected.*/
   public filterString: Array<string> = [];
@@ -27,16 +37,20 @@ export class TrialFilterComponent implements OnInit {
   public teachersList: Array<Array<object>> = [];
   public originalData;
   public dayOfWeekIndex: number = 0;
-  public nodata = 'No Data Found!';
+  public nodata = "No Data Found!";
 
   constructor(
     private coursesService: CoursesService,
     private teachersService: TeachersService,
-    private modalService: NgbModal
-  ) { }
+    private modalService: NgbModal,
+    private transactionService: TransactionService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.AddFilterString(0);
+    if (this.arrangeFlag) {
+      this.getArrangedCourseInstanceDetails();
+    }
   }
 
   /**
@@ -46,76 +60,82 @@ export class TrialFilterComponent implements OnInit {
    * @param item - item object (which item was selected)
    */
   AddFilterString(operationIndex: number, itemIndex?: number, item?: object) {
-    //init get&set course categories filter tags
+    // init get&set course categories filter tags
     if (operationIndex == 0) {
-      this.getCates().subscribe(
-        (res) => {
-          this.filterTags.push(res['Data']);
-        }
-      )
+      if (!this.arrangeFlag) {
+        this.getCates().subscribe(res => {
+          this.filterTags.push(res["Data"]);
+        });
+      } else {
+        this.getCates().subscribe(res => {
+          this.filterTags.push(
+            res["Data"].filter(
+              el =>
+                el.CourseCategoryId ===
+                this.arrangeCourseDetails.Course.CourseCategoryId
+            )
+          );
+        });
+      }
     }
 
-    //course categories filter tags processor
+    // course categories filter tags processor
     if (operationIndex == 1) {
-      //if a cate already selected
+      // if a cate already selected
       if (this.filterString.length >= 1) {
-        return
+        return;
       }
-      //if no cate seleted
+      // if no cate seleted
       else {
-        this.filterString.push(item['CourseCategoryName']);
-        this.cateIdFilter = item['CourseCategoryId'];
-        this.cateName = item['CourseCategoryName'];
-        //get&set orgs filter tags
-        this.getOrgs().subscribe(
-          (res) => {
-            this.filterTags.push(res['Data']);
-          }
-        );
+        this.filterString.push(item["CourseCategoryName"]);
+        this.cateIdFilter = item["CourseCategoryId"];
+        this.cateName = item["CourseCategoryName"];
+        // get&set orgs filter tags
+        this.getOrgs().subscribe(res => {
+          this.filterTags.push(res["Data"]);
+        });
       }
     }
 
-    //orgs filter tags processor
+    // orgs filter tags processor
     if (operationIndex == 2) {
-      //if a org already selected
+      // if a org already selected
       if (this.filterString.length >= 2) {
-        return
+        return;
       }
-      //if no org selected
+      // if no org selected
       else {
-        this.filterString.push(item['Abbr']);
-        this.orgIdFilter = item['OrgId'];
-        this.orgName = item['OrgName'];
-        //get&set day of week filter tags
-        let dayOfWeek = this.getDayOfWeek();
+        this.filterString.push(item["Abbr"]);
+        this.orgIdFilter = item["OrgId"];
+        this.orgName = item["OrgName"];
+        // get&set day of week filter tags
+        const dayOfWeek = this.getDayOfWeek();
         this.filterTags.push(dayOfWeek);
       }
     }
 
-    //day of week filter tags processor
+    // day of week filter tags processor
     if (operationIndex == 3) {
-      //if a day of week already selected
+      // if a day of week already selected
       if (this.filterString.length >= 3) {
-        return
+        return;
       }
-      //if no day of week tag selected
+      // if no day of week tag selected
       else {
         this.filterString.push(item.toString());
-        //get&set teachers (results)
-        //if data already exist, no use to get it again
+        // get&set teachers (results)
+        // if data already exist, no use to get it again
         if (this.originalData) {
           this.processTeachersList(this.originalData, itemIndex);
           return;
         }
-        //no data exist, get it from server
+        // no data exist, get it from server
         else {
-          this.getTeachersNTeachingCourses().subscribe(
-            (res) => {
-              this.originalData = res;
-              //process data got
-              this.processTeachersList(res, itemIndex);
-            }
-          )
+          this.getTeachersNTeachingCourses().subscribe(res => {
+            this.originalData = res;
+            // process data got
+            this.processTeachersList(res, itemIndex);
+          });
         }
       }
     }
@@ -136,56 +156,65 @@ export class TrialFilterComponent implements OnInit {
   }
 
   getDayOfWeek() {
-    return [1, 2, 3, 4, 5, 6, 7, 'All'];
+    return [1, 2, 3, 4, 5, 6, 7, "All"];
+  }
+
+  getArrangedCourseInstanceDetails() {
+    this.transactionService
+      .GroupOr121(this.activatedRoute.snapshot.params.courseId, 0)
+      .subscribe(res => {
+        this.arrangeCourseDetails = res.Data;
+        console.log(this.arrangeCourseDetails);
+        this.AddFilterString(0);
+      });
   }
 
   /**
    * Get teachers from server.
    */
   getTeachersNTeachingCourses() {
-    let getTeachers = this.teachersService.getTeachersInfo();
-    let getTeachingCourses = this.teachersService.getTeachingCourse();
+    const getTeachers = this.teachersService.getTeachersInfo();
+    const getTeachingCourses = this.teachersService.getTeachingCourse();
     return forkJoin([getTeachers, getTeachingCourses]);
   }
 
   /**
    * Process the data of tachers with filters
    * @param data - data to process
-   * @param selectionIndex - index of which teacher selected 
+   * @param selectionIndex - index of which teacher selected
    */
   processTeachersList(data: Array<object>, selectionIndex: any) {
     /**@property {Array<object>} array1 - array after processing (teachers list that pass org filter)*/
-    let array1: Array<object> = [];
-    //data[0] - teachers list
-    data[0]['Data'].map(
-      (val) => {
-        val['AvailableDays'].map(
-          (item) => {
-            if (item.OrgId == this.orgIdFilter) {
-              if (array1.indexOf(val) == -1) {
-                array1.push(val);
-              }
-            }
+    const array1: Array<object> = [];
+    // data[0] - teachers list
+    data[0]["Data"].map(val => {
+      val["AvailableDays"].map(item => {
+        if (item.OrgId == this.orgIdFilter) {
+          if (array1.indexOf(val) == -1) {
+            array1.push(val);
           }
-        )
-      }
-    )
+        }
+      });
+    });
     /**@property {Array<object>} array2 - array after processing (teachers list that pass cate and org filter) */
     let array2: Array<object> = [];
-    array1.map(
-      (val) => {
-        //data[1] - courses teaching list
-        for (let i of data[1]['Data']) {
-          if (i.Course.CourseCategory.CourseCategoryId == this.cateIdFilter && val['TeacherId'] == i.TeacherId) {
-            if (array2.indexOf(val) == -1) {
-              array2.push(val);
-            }
+    array1.map(val => {
+      // data[1] - courses teaching list
+      for (const i of data[1]["Data"]) {
+        if (
+          i.Course.CourseCategory.CourseCategoryId == this.cateIdFilter &&
+          val["TeacherId"] == i.TeacherId
+        ) {
+          if (array2.indexOf(val) == -1) {
+            array2.push(val);
           }
         }
       }
-    )
+    });
+    array2 = array2.filter(
+      el => el["Level"] === this.arrangeCourseDetails.Course.TeacherLevel
+    );
     this.checkTeacherAvailableDays(array2, selectionIndex);
-    //console.log(this.teachersListAfterFilter)
   }
 
   /**
@@ -194,31 +223,28 @@ export class TrialFilterComponent implements OnInit {
    */
   checkTeacherAvailableDays(teacherList: Array<object>, selectionIndex: any) {
     /**@property {Array<Array<object>>} array - list after process*/
-    let array: Array<Array<object>> = [[], [], [], [], [], [], []];
-    teacherList.map(
-      (val) => {
-        for (let i of val['AvailableDays']) {
-          if (array[i.DayOfWeek - 1].indexOf(val) == -1) {
-            array[i.DayOfWeek - 1].push(val);
-          }
+    const array: Array<Array<object>> = [[], [], [], [], [], [], []];
+    teacherList.map(val => {
+      for (const i of val["AvailableDays"]) {
+        if (array[i.DayOfWeek - 1].indexOf(val) == -1) {
+          array[i.DayOfWeek - 1].push(val);
         }
       }
-    );
+    });
 
     this.getTeacherListAfterDayOfWeekFilter(array, selectionIndex);
   }
 
   /**
    * Get teacher list with different day of week.
-   * @param list - 
+   * @param list -
    * @param selectionIndex - index of different day
    */
   getTeacherListAfterDayOfWeekFilter(list, selectionIndex: any) {
     if (selectionIndex == 7) {
       this.dayOfWeekIndex = null;
       this.teachersList = list;
-    }
-    else {
+    } else {
       this.dayOfWeekIndex = selectionIndex;
       this.teachersList = [list[selectionIndex]];
     }
@@ -229,20 +255,25 @@ export class TrialFilterComponent implements OnInit {
    * @param teacher - teacher selected
    */
   popupCalendarModal(teacher: object) {
-    let modalRef = this.modalService.open(TrialCalendarComponent, { size: 'lg', backdrop: 'static', keyboard: false });
+    console.log(this.arrangeCourseDetails);
+    const modalRef = this.modalService.open(TrialCalendarComponent, {
+      size: "lg",
+      backdrop: "static",
+      keyboard: false
+    });
     modalRef.componentInstance.teacher = teacher;
     modalRef.componentInstance.orgName = this.orgName;
     modalRef.componentInstance.orgId = this.orgIdFilter;
     modalRef.componentInstance.CourseCategoryId = this.cateIdFilter;
     modalRef.componentInstance.courseCategoryName = this.cateName;
+    modalRef.componentInstance.durationType = this.arrangeCourseDetails.Course.Duration;
   }
 
   removeFilters(index) {
     if (index == 0) {
       this.filterString = [];
       this.filterTags = [this.filterTags[0]];
-    }
-    else {
+    } else {
       this.filterString = this.filterString.slice(0, index);
       this.filterTags = this.filterTags.slice(0, index + 1);
     }
@@ -250,5 +281,4 @@ export class TrialFilterComponent implements OnInit {
     this.teachersList = [];
     this.dayOfWeekIndex = 0;
   }
-
 }
