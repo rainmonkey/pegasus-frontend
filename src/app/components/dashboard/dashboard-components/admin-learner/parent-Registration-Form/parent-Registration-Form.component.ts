e0563a6404e08a8aace2747c7fd149d95692d0d3
YuyuanDing <dyy1678032782@gmail.com>
2019-08-13 15:35:56 +1200

    1

--- src/app/components/dashboard/dashboard-components/admin-learner/parent-Registration-Form/parent-Registration-Form.component.ts
+++ src/app/components/dashboard/dashboard-components/admin-learner/parent-Registration-Form/parent-Registration-Form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Command } from 'protractor';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { group } from '@angular/animations';
+import { anyKeysRemoved } from '@fullcalendar/core/util/object-similarity';
 
 @Component({
   selector: 'app-parent-Registration-Form',
@@ -7,12 +9,65 @@ import { Command } from 'protractor';
   styleUrls: ['./parent-Registration-Form.component.css']
 })
 export class ParentRegistrationFormComponent implements OnInit {
-@Input() command
-@Input() whichLearner
-@Input() newLearner
-  constructor() { }
+  public parentRegForm: FormGroup
+  parent = []
+  get parentForm() { return this.parentRegForm.get('parentForm') as FormArray; }
+  @Input() command
+  @Input() whichLearner
+  @Input() newLearner
+  constructor(private fb: FormBuilder, ) { }
 
   ngOnInit() {
+    this.parentRegForm = this.fb.group({
+      parentForm: this.fb.array([this.test()])
+    });
+    // this.setParentForm();
   }
 
+
+  // setParentForm(){
+  //   this.parentForm.push(
+  //     this.fb.group({
+  //       FirstName: ['', Validators.required],
+  //       LastName: ['', Validators.required],
+  //       Relationship: ['', Validators.required],
+  //       ContactNum: ['', Validators.required],
+  //       Email: ['', [Validators.required, Validators.email]]
+  //     })
+  //   )
+  // }
+
+  test() {
+    let tempObj: any
+    tempObj=this.fb.group({
+      FirstName: ['', Validators.required],
+      LastName: ['', Validators.required],
+      Relationship: ['', Validators.required],
+      ContactNum: ['', Validators.required],
+      Email: ['', [Validators.required, Validators.email]]
+    })
+    return tempObj
+  }
+
+
+
+
+  resetParent() {
+    this.parentForm.reset();
+  }
+  deleteParent(i) {
+    this.parentForm.removeAt(i);
+  }
+  addParent() {
+    this.parentForm.push(
+      this.fb.group({
+        FirstName: ['', Validators.required],
+        LastName: ['', Validators.required],
+        Relationship: ['', Validators.required],
+        ContactNum: ['', Validators.required],
+        Email: ['', Validators.required, Validators.email],
+      })
+    );
+    // console.log('addParent', this.parentForm.value)
+  }
 }
