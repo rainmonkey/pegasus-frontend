50eaf8498f3453440147d599a7775f3efd2d3140
YuyuanDing <dyy1678032782@gmail.com>
2019-09-11 14:16:11 +1200

    1

--- src/app/components/dashboard/dashboard-components/admin-learner/admin-learner-list/admin-learner-list.component.ts
+++ src/app/components/dashboard/dashboard-components/admin-learner/admin-learner-list/admin-learner-list.component.ts
@@ -330,9 +330,32 @@ export class AdminLearnerListComponent implements OnInit {
     modalRef.componentInstance.whichLearner = whichLearner;
   }
 
-  regiModal(command,whichLearner) {
+  newRegiModal(command,whichLearner) {
     //@ts-ignore
     const modalRef = this.modalService.open(NewLearnerRegistrationModalComponent, { size: 'xl', backdrop: 'static', keyboard: false });
+
+    let that = this;
+    modalRef.result.then(
+      (res) => {
+        that.ngOnInit()
+      },
+      (err) => {
+        return
+      }
+    )
+    modalRef.componentInstance.command = command;
+    modalRef.componentInstance.whichLearner = whichLearner;
+    modalRef.componentInstance.refreshFlag.subscribe(res => {
+      if (res == true) {
+        that.ngOnInit();
+      }
+    })
+
+  }
+
+  regiModal(command,whichLearner) {
+    //@ts-ignore
+    const modalRef = this.modalService.open(LearnerRegistrationFormComponent, { size: 'xl', backdrop: 'static', keyboard: false });
 
     let that = this;
     modalRef.result.then(
