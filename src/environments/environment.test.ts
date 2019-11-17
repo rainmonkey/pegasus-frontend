37a181f3515129aa13d12f966e707f3540c86fec
Yinghui Zhu <edwin.zhu02@gmail.com>
2019-11-17 22:11:43 +1300

    evironment

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -9,3 +9,17 @@ export const environment = {
     companyName: 'Able Music Studio',
     siteName: 'Project Pegasus'
   };
+
+  
+export const instrumentIcon =[
+  {id:0,name:'Group',url:'../../../../../../Group.png'},
+  {id:1,name:'Piano',url:'../../../../../../Piano.svg'},
+  {id:2,name:'Drum',url:'../../../../../../Drum.png'},
+  {id:3,name:'Guitar',url:'../../../../../../Guitar.png'},
+  {id:4,name:'Violin',url:'../../../../../../Violin.png'},
+  {id:5,name:'Cello',url:'../../../../../../Cello.png'},
+  {id:6,name:'Singing',url:'../../../../../../voice.png'},
+  {id:7,name:'Theory',url:'../../../../../../Theory.png'},
+  {id:8,name:'Aural',url:'../../../../../../Aural.svg'},
+  {id:7,name:'Others',url:'../../../../../../Others.svg'},
+]
